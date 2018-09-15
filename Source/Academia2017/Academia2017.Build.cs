4d8033cb0c4fe74915912af101c0c08633b770b2
PatriceVignola <vignola.patrice@gmail.com>
2018-09-15 01:13:29 -0700

    Update build C# files to 4.20

--- Source/Academia2017/Academia2017.Build.cs
+++ Source/Academia2017/Academia2017.Build.cs
@@ -4,7 +4,7 @@ using UnrealBuildTool;
 
 public class Academia2017 : ModuleRules
 {
-	public Academia2017(TargetInfo Target)
+	public Academia2017(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "PhysX", "APEX", "UMG", "Slate", "SlateCore", "OnlineSubsystem", "OnlineSubsystemUtils" });
 	}
--- Source/Academia2017Editor.Target.cs
+++ Source/Academia2017Editor.Target.cs
@@ -5,21 +5,9 @@ using System.Collections.Generic;
 
 public class Academia2017EditorTarget : TargetRules
 {
-	public Academia2017EditorTarget(TargetInfo Target)
+	public Academia2017EditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-	}
-
-	//
-	// TargetRules interface.
-	//
-
-	public override void SetupBinaries(
-		TargetInfo Target,
-		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
-		ref List<string> OutExtraModuleNames
-		)
-	{
-		OutExtraModuleNames.Add("Academia2017");
+		ExtraModuleNames.Add("Academia2017");
 	}
 }
